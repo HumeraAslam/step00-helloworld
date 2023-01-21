408a7664c73e63c7fd0d300dccd39d62755d5182
Humera Aslam <soniahumera@gmail.com>
2023-01-21 15:26:06 +0500

    Hello world

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
- import Image from 'next/image'
- import { Inter } from '@next/font/google'
- import styles from './page.module.css'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from './page.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return(
+  return (
     <center>
       <div> 
-        <h1>Hello World <br /></h1>
-        <h4>from <br /></h4>
-        <h2> <b> Humera Aslam</b></h2>
+      <h1>Hello World <br /></h1>
+      <h4>from <br /></h4>
+      <h2> <b> Humera Aslam</b></h2>
       </div>
-    </center>
+  </center>
   )
 }
